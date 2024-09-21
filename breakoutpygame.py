f4e3eddd875204ddbcb93bff9db972e3b42447e3
Arthur <amcr.snf24@uea.edu.br>
2024-09-21 10:35:27 -0400

    Wall blocks collision and ball initial position added

--- breakoutpygame.py
+++ breakoutpygame.py
@@ -21,12 +21,12 @@ pygame.display.set_caption("Breakout - PyGame Edition - 2024-09-17")
 
 # level text
 text_font = pygame.font.Font('assets/pong-score.ttf', 45)
-level_text = text_font.render("1" , True, COLOR_WHITE)
+level_text = text_font.render("1", True, COLOR_WHITE)
 level_text_rect = level_text.get_rect()
 level_text_rect.center = (40, 50)
 
 # try text
-try_text = text_font.render("1" , True, COLOR_WHITE)
+try_text = text_font.render("1", True, COLOR_WHITE)
 try_text_rect = try_text.get_rect()
 try_text_rect.center = (350, 50)
 
@@ -34,31 +34,31 @@ try_text_rect.center = (350, 50)
 score_font = pygame.font.Font('assets/pong-score.ttf', 50)
 score_text = score_font.render('005', True, COLOR_WHITE)
 score_text_rect = score_text.get_rect()
-score_text_rect.center = (120,100)
+score_text_rect.center = (120, 100)
 
 # score 2 text
 score_2_text = score_font.render('000', True, COLOR_WHITE)
 score_2_text_rect = score_2_text.get_rect()
-score_2_text_rect.center = (430,100)
+score_2_text_rect.center = (430, 100)
 
 # Text to start the game
 start_font = pygame.font.Font('assets/PressStart2P.ttf', 15)
-start_text = start_font.render("Press ANY KEY to start the game" , True, COLOR_WHITE)
+start_text = start_font.render("Press ANY KEY to start the game", True, COLOR_WHITE)
 start_text_rect = start_text.get_rect()
 start_text_rect.center = (290, 550)
 
 # player 1
 player_1 = pygame.image.load("assets/player.png")
-player_1_width = 50   # Width of the player
+player_1_width = 50  # Width of the player
 player_1_height = 15  # Height of the player
-player_1_x = (WIDTH_SCREEN/2) - 25        # Initial x position
-player_1_y = HEIGHT_SCREEN - 60   # y position near the bottom of the screen
+player_1_x = (WIDTH_SCREEN / 2) - 25  # Initial x position
+player_1_y = HEIGHT_SCREEN - 60  # y position near the bottom of the screen
 player_1_move_right = False
 player_1_move_left = False
 
 # Create ball
 ball = pygame.image.load("assets/ball.png")
-ball_x = (WIDTH_SCREEN/2) - 5
+ball_x = (WIDTH_SCREEN / 2) - 5
 ball_y = HEIGHT_SCREEN - 70
 ball_width = 10
 ball_height = 10
@@ -70,10 +70,11 @@ ball_area = ball_width * ball_height
 BLOCK_WALL_COLUMN = 14
 BLOCK_WALL_ROWS = 8
 
+
 # Create the wall of blocks
 class BLOCKS:
     def __init__(self):
-        self.width = (WIDTH_SCREEN - 12)//BLOCK_WALL_COLUMN
+        self.width = (WIDTH_SCREEN - 12) // BLOCK_WALL_COLUMN
         self.height = 13
 
     def create_walls(self):
@@ -108,6 +109,7 @@ class BLOCKS:
                 pygame.draw.rect(screen, color_block, block)
                 pygame.draw.rect(screen, COLOR_BLACK, block, 2)
 
+
 wall_block = BLOCKS()
 wall_block.create_walls()
 
@@ -116,19 +118,28 @@ game_loop = True
 game_clock = pygame.time.Clock()
 interval = True
 
+
 # collision check function
-def collision_check(width, height):
-    global ball_x, ball_y, ball_dx, ball_dy, WIDTH_SCREEN, HEIGHT_SCREEN
-    if ball_x >= WIDTH_SCREEN - width:
-        ball_x *= -1
+def collision_check(wall_blocks):
+    global ball_x, ball_y, ball_dx, ball_dy
+    if (wall_blocks.top < ball_y + 4 < wall_blocks.top + wall_blocks.height) and (
+            wall_blocks.left < ball_x < wall_blocks.left + wall_blocks.width):
+        ball_dy *= -1
         return True
+    return False
+
 
 while game_loop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_loop = False
         elif event.type == pygame.KEYDOWN:
-            interval = False
+            if interval:
+                ball_x = (WIDTH_SCREEN / 2) + 200
+                ball_y = HEIGHT_SCREEN - 350
+                ball_dx = 5
+                ball_dy = 5
+                interval = False
             if event.key == pygame.K_LEFT:
                 player_1_move_left = False
             if event.key == pygame.K_RIGHT:
@@ -140,9 +151,15 @@ while game_loop:
             if event.key == pygame.K_RIGHT:
                 player_1_move_right = True
 
+    # function check the collision and destruction of each block
+    for i in wall_block.block_wall:
+        for coloured_block in i:
+            collision_check(coloured_block)
+
     if not interval:
         # ball collision with player 1
-        if (player_1_y < ball_y + 4 < player_1_y + player_1_height) and (player_1_x < ball_x < player_1_x + player_1_width):
+        if (player_1_y < ball_y + 4 < player_1_y + player_1_height) and (
+                player_1_x < ball_x < player_1_x + player_1_width):
             ball_dy *= -1
     else:
         # ball collision with a bottom line
@@ -171,19 +188,22 @@ while game_loop:
     ball_x = ball_x + ball_dx
     ball_y = ball_y + ball_dy
 
-    # ball collides with wall
+    # ball collides with bottom border
     if ball_y >= HEIGHT_SCREEN:
         ball_dy *= -1
         ball_y = HEIGHT_SCREEN - ball_height
 
+    # ball collides with top border
     if ball_y <= 0:
         ball_dy *= -1
         ball_y = 0
 
+    # ball collides with right border
     if ball_x >= WIDTH_SCREEN - ball_width:
         ball_dx *= -1
         ball_x = WIDTH_SCREEN - ball_width
 
+    # ball collides with left border
     if ball_x <= 0:
         ball_dx *= -1
         ball_x = 0
@@ -199,14 +219,15 @@ while game_loop:
         pygame.draw.rect(screen, COLOR_YELLOW, (width, 205, 10, 25))
         pygame.draw.rect(screen, COLOR_PADDLE, (width, HEIGHT_SCREEN - 67, 10, 30))
 
+
     # Draw block wall
     wall_block.draw_walls()
     # Draw white borders: top, left, and right
     pygame.draw.rect(screen, COLOR_WHITE, (0, 0, WIDTH_SCREEN, 25))  # Top border
     pygame.draw.rect(screen, COLOR_WHITE, (0, 0, 10, HEIGHT_SCREEN))  # Left border
-    wall_colors(0) # Left border color
+    wall_colors(0)  # Left border color
     pygame.draw.rect(screen, COLOR_WHITE, (WIDTH_SCREEN - 10, 0, 10, HEIGHT_SCREEN))  # Right border
-    wall_colors(WIDTH_SCREEN - 10) # Left border color
+    wall_colors(WIDTH_SCREEN - 10)  # Left border color
 
     # Drawing texts
     screen.blit(level_text, level_text_rect)
@@ -219,11 +240,10 @@ while game_loop:
 
     if interval:
         # Draw start line
-        pygame.draw.rect(screen, COLOR_PADDLE, (0, HEIGHT_SCREEN - 60, WIDTH_SCREEN, 15)) # Top border
+        pygame.draw.rect(screen, COLOR_PADDLE, (0, HEIGHT_SCREEN - 60, WIDTH_SCREEN, 15))  # Top border
         sline_x = HEIGHT_SCREEN - 60
         sline_y = WIDTH_SCREEN
         screen.blit(start_text, start_text_rect)
-        # collision_check(sline_y, sline_x)
     else:
         pygame.draw.rect(screen, COLOR_PADDLE, (player_1_x, player_1_y, player_1_width, player_1_height))
 
